ec2b37db325cfafe57ac3802dd99fad65d121462
Saverio Proto <saverioproto@microsoft.com>
2022-06-01 12:16:27 +0200

    drop checks on not existing examples folder

--- scripts/terraform-validate.sh
+++ scripts/terraform-validate.sh
@@ -2,11 +2,6 @@
 echo "==> Validating module terraform codes..."
 error=false
 (terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
-echo "==> Checking examples terraform codes are validate..."
-examples=$(find ./examples -maxdepth 1 -mindepth 1 -type d)
-for d in $examples; do
-  (echo "===> Terraform validating in " $d && cd $d && rm -f .terraform.lock.hcl && terraform init -upgrade && terraform validate -json | jq -e .valid) || error=true
-done
 if ${error}; then
   echo "------------------------------------------------"
   echo ""

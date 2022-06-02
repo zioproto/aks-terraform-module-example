376570c7ecda469a4bd284ab51c5be809bf1d3eb
Saverio Proto <saverioproto@microsoft.com>
2022-06-02 09:29:33 +0200

    I dont have an examples folder in this repo

--- scripts/run-tflint.sh
+++ scripts/run-tflint.sh
@@ -14,17 +14,4 @@ if ${error}; then
   echo ""
   exit 1
 fi
-cd examples
-files=$(find * -type f -name "*.tf" | grep -v ".terraform")
-error=false
-for f in $files; do
-  tflint --config=../.tflint.hcl --disable-rule=terraform_documented_outputs --disable-rule=terraform_documented_variables "$f" || error=true
-done
-if ${error}; then
-  echo "------------------------------------------------"
-  echo ""
-  echo "The preceding files contain terraform blocks that does not complies with tflint requirements."
-  echo ""
-  exit 1
-fi
 exit 0
